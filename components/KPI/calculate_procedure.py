643d026c7ba15c58acceea9b79c8cd2e3131764e
Elijah Mylcev <elijahmylcev@gmail.com>
2023-06-13 01:14:07 +0200

    create calculate_procedure for kpi

--- components/KPI/calculate_procedure.py
+++ components/KPI/calculate_procedure.py
@@ -1,67 +1,62 @@
 import pandas as pd
-import dash_core_components as dcc
-import dash_html_components as html
-from functions import get_agents
-import dash_bootstrap_components as dbc
-import plotly.express as px
+from datetime import datetime
+from sqlalchemy import text
+from functions import get_engine
+from dateutil.relativedelta import relativedelta
 
 
-class KPI:
-    def __init__(self, text, engine, agent=13):
-        self.text = text
+class CalculateKPI:
+    """
+    Class for calculate statistic
+    """
+    def __init__(self, output_file, engine):
+        self.now_month = datetime.now().month
+        self.now_year = datetime.now().year
+        self.out = output_file
         self.engine = engine
-        self.df = None
-        self.agent = agent
+        self.users = self.get_unique_users()
+        self.date_range = self.get_range()
 
-    def process(self):
-        # df = self.execute_query(self.agent, 2022)
-        self.df = pd.read_csv('components/KPI/KPI_result.csv')
-        available_agents = get_agents('fos_user', self.engine)
-        available_metrics = self.df.columns
-        return html.Div([
-            html.H2(self.text, style={'text-align': 'center'}),
-            html.Div([
-                html.Label('Выберите менеджера:'),
-                dcc.Dropdown(
-                    id='agent-dropdown',
-                    options=[{
-                        'label': agent[1]['username'],
-                        'value': agent[1]['user_id']
-                    }
-                        for agent in available_agents.iterrows()],
-                    value=available_metrics[0],
-                    multi=False
-                ),
-            ]),
-            html.Div([
-                html.Label('Выберите метрики:'),
-                dcc.Dropdown(
-                    id='metric-dropdown',
-                    options=[{'label': metric, 'value': metric} for metric in available_metrics],
-                    value=available_metrics[2],
-                    multi=True
-                ),
-            ]),
-            dcc.Graph(id='kpi'),
-        ])
+    def process(self) -> None:
+        df_list = []
+        for _date in self.date_range:
+            for user in self.users:
+                query = self.get_request(_date.month, _date.year, user)
+                df_list.append(pd.read_sql(query, self.engine))
+        df = pd.concat(df_list)
+        try:
+            df.to_csv(self.out)
+        except Exception as e:
+            print(str(e))
 
-    def execute_query(self, id_agent, year):
+    def get_start(self) -> datetime:
+        query = text("SELECT MIN(created_at) AS earliest_date FROM deals;")
+        result = self.engine.execute(query)
+        earliest_date = result.scalar()
+        return earliest_date
+
+    def get_unique_users(self) -> list:
+        query = text("SELECT distinct user_id from deals;")
+        result = self.engine.execute(query)
+        users = [row[0] for row in result.fetchall()]
+        filtered_users = list(filter(lambda x: x is not None, users))
+        return filtered_users
+
+    def get_range(self) -> list:
         """
-        return result df in year
-        :param id_agent: agent id
-        :param year: year for analyse
-        :return: df
-        """
-        dataframes = []
-        for month in list(range(1, 13)):
-            dataframes.append(pd.read_sql(self.get_query(month, year, id_agent), self.engine))
-        result_df = pd.concat(dataframes)
-        self.df = result_df
-        return result_df
 
+        :return: Массив дат от старта до предыдущего месяца с шагом 1 месяц
+        """
+        end_date = datetime.now() - relativedelta(months=1)
+        date_range = []
+        current_date = self.get_start()
+        while current_date < end_date:
+            date_range.append(current_date)
+            current_date += relativedelta(months=1)
+        return date_range
 
     @staticmethod
-    def get_query(month, year, id_agent):
+    def get_request(month, year, id_agent) -> str:
         return f'''
             SELECT * FROM (
                 SELECT 
@@ -139,3 +134,8 @@ class KPI:
             ) AS y
             on x.id_agent = y.id_agent
         '''
+
+
+if __name__ == '__main__':
+    date = CalculateKPI(output_file='KPI_result.csv', engine=get_engine())
+    print(date.process())
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
+import plotly.graph_objects as go
 from functions import get_df_from_table, get_agents, get_engine
 from components import get_table, create_funnel, KPI
 
@@ -25,7 +26,7 @@ tab_1 = html.Div([
 ])
 
 tab_2 = create_funnel()
-KPI = KPI('test text')
+KPI = KPI('KPI', engine)
 
 app.layout = html.Div([
   html.Div([
@@ -34,10 +35,105 @@ app.layout = html.Div([
   dcc.Tabs(id='tabs', value='tab-1', children=[
     dcc.Tab(label='Рейтинговая доска', value='tab-1', children=tab_1),
     dcc.Tab(label='Воронка продаж', value='tab-2', children=tab_2),
-    dcc.Tab(label='KPI', value='tab-3', children=KPI.process())
+    dcc.Tab(label='KPI', value='tab-3', children=KPI.process()),
+    dcc.Store(id='initial-data', data=KPI.df.to_json(orient='records'), storage_type='memory'),
+    dcc.Store(id='filtered-data', storage_type='memory'),
+    dcc.Store(id='selected-metrics', data=KPI.df.columns[3], storage_type='memory')
   ])
 ])
 
 
+@app.callback(
+    dash.Output('filtered-data', 'data'),
+    dash.Input('agent-dropdown', 'value'),
+    dash.State('initial-data', 'data')
+)
+def update_graph(selected_agents, initial_data):
+    # Преобразование JSON-строки в исходный датафрейм
+    initial_df = pd.read_json(initial_data, orient='records')
+    # Фильтрация датафрейма по указанному идентификатору
+    filtered_df = initial_df[initial_df['id_agent'] == selected_agents].copy()
+    return filtered_df.to_json(orient='records')
+
+
+# Callback-функция для обновления графика
+@app.callback(
+    dash.Output('kpi', 'figure'),
+    dash.Input('filtered-data', 'data'),
+    dash.State('selected_metrics', 'data')
+)
+def update_graph(data, selected_metrics):
+    if data is None:
+        # Если данные не доступны, отображаем пустой график
+        return {}
+    # Преобразование JSON-строки в фильтрованный датафрейм
+    filtered_df = pd.read_json(data, orient='records')
+    data = []
+    for metric in selected_metrics:
+        for column in KPI.df.columns:
+            if column == metric:
+                scatter = go.Scatter(
+                    x=KPI.df['month'],
+                    y=filtered_df[column],
+                    mode='lines+markers',
+                    name=metric
+                )
+                data.append(scatter)
+
+    # Создание объекта Figure с использованием списка Scatter
+    fig = go.Figure(data=data)
+
+    # Настройка внешнего вида графика
+    fig.update_layout(
+        plot_bgcolor='white',
+        showlegend=True,
+        legend=dict(
+            x=0,
+            y=1
+        ),
+        hovermode='x'
+    )
+
+    return fig
+
+
+@app.callback(
+    dash.Output('filtered_data', 'data'),
+    dash.Input('metric-dropdown', 'value'),
+    dash.State('filtered_data', 'data')
+)
+def update_graph(selected_metrics):
+    # Создание списка объектов Scatter для каждой выбранной метрики
+    # data = []
+    # for metric in selected_metrics:
+    #     for column in KPI.df.columns:
+    #         if column == metric:
+    #             filtered_df = KPI.df[column]
+    #             print(filtered_df)
+    #             scatter = go.Scatter(
+    #                 x=KPI.df['month'],
+    #                 y=filtered_df,
+    #                 mode='lines+markers',
+    #                 name=metric
+    #             )
+    #             data.append(scatter)
+    #
+    # # Создание объекта Figure с использованием списка Scatter
+    # fig = go.Figure(data=data)
+    #
+    # # Настройка внешнего вида графика
+    # fig.update_layout(
+    #     plot_bgcolor='white',
+    #     showlegend=True,
+    #     legend=dict(
+    #         x=0,
+    #         y=1
+    #     ),
+    #     hovermode='x'
+    # )
+
+    return selected_metrics
+
+
 if __name__ == '__main__':
-  app.run_server(debug=True)
+    app.run_server(debug=True)
